6dc90924cfe345622f72abcbe55014f6eeb36391
Kevin Soto <43228622+kevinsoto832@users.noreply.github.com>
2022-10-12 19:54:27 -0500

    Create display-image.php

---
+++ display-image.php
@@ -0,0 +1,6 @@
+<?php
+//These are different ways to display images in wordpress
+
+<figure><img src="http://nu-cafe-updated.local/wp-content/uploads/2022/08/DSC03702-1.jpg" alt="Shoes" /></figure>
+<div class="absolute slide w-64 h-64 bg-no-repeat bg-cover bg-center" style="background-image: url(http://nu-cafe-updated.local/wp-content/uploads/2022/08/DSC03702-1.jpg);"></div>
+<figure class="h-[14.75rem] w-[23.5rem] bg-no-repeat bg-cover bg-center" style="background-image: url(http://nu-cafe-updated.local/wp-content/uploads/2022/08/DSC03695-1.jpg);"></figure>
